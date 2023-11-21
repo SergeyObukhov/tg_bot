e6437a07bb84245d02b2966fce44ad2eb2d45db2
SergeyObukhov <obukhov.sv@yandex.ru>
2023-11-21 09:50:55 +0300

    finished_echo_bot

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tg_bot
 telegram bot development
+
+modular_echo_bot - первый подход к изучению телеграм ботов. Изучение роутеров.
--- modular_echo_bot/bot.py
+++ modular_echo_bot/bot.py
@@ -1,12 +1,28 @@
-from config_data.config import load_config
+import asyncio
 
-# можно передать путь к .env, если файл находится не в директории проекта
-config = load_config()
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+from handlers import other_handlers, user_handlers
 
-bot_token = config.tg_bot.token           # Сохраняем токен в переменную bot_token
 
-# Выводим значения полей экземпляра класса Config на печать, 
-# чтобы убедиться, что все данные, получаемые из переменных окружения, доступны
-print('BOT_TOKEN:', config.tg_bot.token)
-print()
-print('config type:', type(config))
+# Функция конфигурирования и запуска бота
+async def main():
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+    
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
